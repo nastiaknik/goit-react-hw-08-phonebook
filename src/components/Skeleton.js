bb30ee5f39d9650b5b4782631fb99d0402f1da47
nastiaknik <anastazja.knihnitska@gmail.com>
2023-04-30 05:52:29 +0200

    add spinner

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,3 +1,19 @@
+import { ThreeDots } from 'react-loader-spinner';
+
 export const Loader = () => {
-  return <div>Loading</div>;
+  return (
+    <ThreeDots
+      height="80"
+      width="80"
+      radius="9"
+      color="#4fa94d"
+      ariaLabel="three-dots-loading"
+      visible={true}
+      wrapperStyle={{
+        margin: '0 auto',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    />
+  );
 };
